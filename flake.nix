c94c5c9fb1e1dd63ec851b877476c5a131347f8d
marselst <marselst412@gmail.com>
2026-03-05 23:27:56 +0300

    fix: use <= instead of < in max_item_id filter

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
             '';
             commandGroups = {
               "1-front-tools" = [
-                pkgs.nodejs_25
+                pkgs.nodejs_22
               ];
               "2-back-tools" = [
                 pkgs.uv
@@ -44,7 +44,6 @@
                       --exclude-path '.venv' \
                       --exclude-path '.direnv' \
                       --exclude-path 'node_modules' \
-                      --exclude-path 'tmp' \
                       --root-dir . \
                       --cache \
                       --format json \
@@ -57,12 +56,7 @@
                   name = "lint-docs";
                   command = ''
                     ${pkgs.lib.getExe pkgs.markdownlint-cli2} \
-                      '**/*.md' \
-                      '#.direnv' \
-                      '#.venv' \
-                      '#**/node_modules' \
-                      '#*/skills' \
-                      '#tmp'
+                      '**/*.md' '#.direnv' '#.venv' '#node_modules'
                   '';
                   help = "Lint all Markdown files";
                 }
